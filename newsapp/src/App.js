a7d943f1c5142f0845453caf2fa840bc0821b666
Kushal1715 <kushalkhadka77@gmail.com>
2023-12-17 22:00:54 +0545

    displaying dynamic data using state

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import Navbar from './Components/Navbar'
+import News from './Components/News';
 
 export class App extends Component {
   render() {
     return (
-      <div>Class based component</div>
+      <div>
+      <Navbar/>
+      <News/>
+      </div>
     )
   }
 }
---
+++ newsapp/src/Components/NewsItem.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+
+  render() {
+    let { title, desc, imageURL, url } = this.props;
+    return (
+      <div className='my-3'>
+        <div className="card" style={{ width: "18rem" }}>
+          <img style={{width:"286px", height: "200px"}}className="card-img-top" src={imageURL} alt='' />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{desc}...</p>
+            <a href={url} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary">Read More</a>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default NewsItem 
